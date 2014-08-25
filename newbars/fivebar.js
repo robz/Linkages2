dad7b7949863eba2b6bd6a0441caf52b083f3d0b
robz <geomasta21@aol.com>
2014-08-24 19:29:46 -0700

    added beautiful refactored and untested bars

---
+++ newbars/fivebar.js
@@ -0,0 +1,38 @@
+var Fivebar = function (p1x, p1y, p5x, p5y, a, b, c, d) {
+  this.p1x = p1x;
+  this.p1y = p1y;
+  this.p5x = p5x;
+  this.p5y = p5y;
+  this.a = a;
+  this.b = b;
+  this.c = c;
+  this.d = d;
+  this.Fourbar = Fourbar;
+};
+
+Fivebar.prototype.calcPoints = function (theta1, theta2) {
+  var P14angle = Math.atan2(p4y - p1y, p4x - p1x);
+  var P15angle = Math.atan2(p5y - p1y, p5x - p1x);
+  var diff = P14angle - P15angle;
+  var fourbarTheta1 = (theta1 - diff + Math.PI * 2) % (Math.PI * 2);
+  
+  var P45angle = theta2 + P15angle;
+  this.p4x = this.p5x + this.d * Math.cos(P45angle);
+  this.p4y = this.p5y + this.d * Math.sin(P45angle);
+
+  var fbpoints = this.Fourbar.prototype.calcPoints.call(this, fourbarTheta1);
+
+  return fbpoints.concat([this.p4x, this.p4y]);
+};
+
+
+var FivebarExt = function(p1x, p1y, p5x, p5y, a, b, c, d, thetaExt, dExt) {
+  Fivebar.call(this, p1x, p1y, p5x, p5y, a, b, c, d);
+  this.dExt = dExt;
+  this.thetaExt = thetaExt;
+  this.Fourbar = FourbarExt;
+};
+
+FivebarExt.prototype = new Fivebar();
+FivebarExt.prototype.constructor = FivebarExt;
+
---
+++ newbars/fourbar.js
@@ -0,0 +1,93 @@
+var Fourbar = function (p1x, p1y, p4x, p4y, a, b, c) {
+  this.p1x = p1x;
+  this.p1y = p1y;
+  this.p4x = p4x;
+  this.p4y = p4y;
+  this.a = a;
+  this.b = b;
+  this.c = c;
+};
+        
+Fourbar.prototype.calcPoints = (function {
+  function acos(x) {
+    if (x > 1 + 1e-6 || x < -1 - 1e-6) { throw "acos(" + x + ")"; }
+    return Math.acos(x);
+  }
+            
+  function sqrt(x) {
+    if (x < 0) { throw "sqrt(" + x + ")"; }
+    return Math.sqrt(x);
+  }
+
+  function euclid(p1x, p1y, p2x, p2y) {
+    var dx = p2x - p1x;
+    var dy = p2y - p1y;
+    return Math.sqrt(dx*dx + dy*dy);
+  }
+
+  return function (theta1) {
+    // convienence definitions
+    var a = this.a;
+    var b = this.b;
+    var c = this.c;
+    var d = euclid(this.p1x, this.p1y, this.p2x, this.p2y);
+
+    // position analysis (using geometric method)
+    var f = sqrt(a * a + d * d - 2 * a * d * Math.cos(theta1));
+    var phi = acos((f * f + d * d - a * a) / (2 * f * d));
+    var gamma = acos((f * f + c * c - b * b) / ( 2 * f * c));
+
+    if (theta1 < Math.PI) {
+        var theta3 = Math.PI - (gamma + phi);
+    } else {
+        var theta3 = Math.PI - (gamma - phi);
+    }
+
+    var alpha = acos((b * b + c * c - f * f)/(2 * b * c));
+    var theta2 = theta3 - alpha;
+    
+    // now calculate the points 
+    var p2x = p1x + a * Math.cos(zeta + theta2);
+    var p2y = p1y + a * Math.sin(zeta + theta2);
+    
+    var p3x = p2x + b * Math.cos(zeta + theta3);
+    var p3y = p2y + b * Math.sin(zeta + theta3);
+    
+    // consistency check
+    var p3x_2 = p4x + c * Math.cos(zeta + theta4);
+    var p3y_2 = p4y + c * Math.sin(zeta + theta4);
+    
+    if (Math.abs(p3x_2 - p3x) > 1e-6
+        || Math.abs(p3y_2 - p3y) > 1e-6) 
+    {
+        throw "error: consistency calculation for p3 failed";
+    }
+
+    // save theta3 for FourbarExt calculation
+    // this.P23angle = theta3;
+
+    return [p2x, p2y, p3x, p3y];
+  };
+}());
+
+
+var FourbarExt = function (p1x, p1y, p4x, p4y, a, b, c, thetaExt, dExt) {
+  Fourbar.call(this, p1x, p1y, p4x, p4y, a, b, c);
+  this.dExt = dExt;
+  this.thetaExt = thetaExt;
+};
+
+FourbarExt.prototype = new Fourbar();
+FourbarExt.prototype.constructor = FourbarExt;
+
+FourbarExt.prototype.calcPoints = function (input1) {
+  var points = Fourbar.prototype.calcPoints.call(input1);
+
+  this.P23angle = Math.atan2(points[3] - points[1], points[2] - points[0]);
+
+  var pEx = points[0] + this.dExt * Math.cos(this.thetaExt + this.P23angle);
+  var pEy = points[1] + this.dExt * Math.sin(this.thetaExt + this.P23angle);
+
+  return points.concat([pEx, pEy]);
+};
+
